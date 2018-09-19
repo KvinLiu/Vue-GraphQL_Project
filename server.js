e22722ec7ec569806701e552b5a5bff337563e88
Kevin <kevin07liu@gmail.com>
2018-09-19 13:45:50 +0800

    Feat: Add new mutation addPost

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const resolvers = require('./resolvers');
+const typeDefs = requireText('./typeDefs.graphql', require);
 
 const User = require('./models/User');
 const Post = require('./models/Post');
@@ -14,9 +15,6 @@ mongoose
   .connect(process.env.MOGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB connected'))
   .catch(err => console.err(err));
-
-const typeDefs = requireText('./typeDefs.graphql', require);
-// const typeDefs = gql(schema);
 
 const server = new ApolloServer({
   typeDefs,
