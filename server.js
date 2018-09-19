90c25194517949dfea1ab1f886523b55836a48c7
Kevin <kevin07liu@gmail.com>
2018-09-19 15:43:38 +0800

    Feat: Add Frontend Client powered by Vue-Cli3

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const requireText = require('require-text');
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
+// Import typeDefs and resolvers
 const resolvers = require('./resolvers');
 const typeDefs = requireText('./typeDefs.graphql', require);
 
+// Improt Enviroment Variables and Mongoose models
+require('dotenv').config({ path: 'variables.env'});
 const User = require('./models/User');
 const Post = require('./models/Post');
 
-require('dotenv').config({ path: 'variables.env'});
 
+// Connect to Mlab Database
 mongoose
   .connect(process.env.MOGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB connected'))
   .catch(err => console.err(err));
 
+// Create Apollo/GraphQL Server using typeDefs, resolvers and context
+// object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
